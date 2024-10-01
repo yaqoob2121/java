3a2298818c82fcda5e8b0a2643c59d7cc0064e27
yaqoob2121 <yaqoob2131mohd@gmail.com>
2024-10-01 11:11:27 +0100

    pushing code

---
+++ Not_Yet_Decided.java
@@ -0,0 +1,11 @@
+public class Not_Yet_Decided{
+
+       public static void main(String args[]){
+       
+         System.out.println(" Not_Yet_Decided");
+
+
+
+       }
+
+}
---
+++ Total_Price.java
@@ -0,0 +1,14 @@
+public class Total_Price{
+
+          public static void main(String args[]){
+
+             
+              String productName="Mobiles";
+              int price=5000;
+              int numberofproducts=20;
+              int totalprice=price*numberofproducts;
+              System.out.println("product name:"+productName);
+              System.out.println("total price is:"+totalprice);
+              }
+
+}
