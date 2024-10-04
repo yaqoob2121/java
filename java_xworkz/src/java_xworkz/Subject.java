36804ed4d63eddc6aeb1574c87c2173dc53e6251
yaqoob2121 <yaqoob2131mohd@gmail.com>
2024-10-04 11:32:12 +0100

    pushing code

---
+++ java_xworkz/src/java_xworkz/Subject.java
@@ -0,0 +1,43 @@
+package java_xworkz;
+
+public class Subject {
+
+	// properties of subject
+	public String SubjectName;
+	public String SubjectCode;
+
+	// constructor
+	public Subject() {
+		System.out.println("default constructor");
+	}
+
+	// instance methods
+	public void getSubjectDetails() {
+		System.out.println("this is getSubjectDetails");
+	}
+
+	public void getSubjectDetails(String SubjectName) {
+		System.out.println("this is getSubjectDetails method");
+		System.out.println("subjectName:" + SubjectName);
+
+	}
+	public void getSubjectDetails(String SubjectName,String SubjectCode) {
+		System.out.println("this is getSubjectDetails");
+		System.out.println("subjectName:" + SubjectName);
+		System.out.println("this is getSubjectDetails");
+		System.out.println("subjectcode:" + SubjectCode);
+		}
+       
+	//to get length of given subject
+	public void NumberOfChar(String SubjectName) {
+		System.out.println("finding the char count");
+		System.out.println(" the count is:"+SubjectName.length());
+	}
+	public void NumberOfChar(String SubjectName,String SubjectCode) {
+		System.out.println("finding the char count");
+		System.out.println(" the count is:"+SubjectName.length());
+		System.out.println("the count is:"+SubjectCode.length());
+	}
+	
+
+}
---
+++ java_xworkz/src/java_xworkz/SubjectRunner.java
@@ -0,0 +1,16 @@
+package java_xworkz;
+
+public class SubjectRunner {
+
+	public static void main(String[] args) {
+		Subject Subject1 = new Subject ();
+		Subject1.getSubjectDetails();
+		Subject1.getSubjectDetails("maths");
+		Subject1.getSubjectDetails("basic signal processing");
+		Subject1.getSubjectDetails("21EC32");
+		Subject1.NumberOfChar("sunskrutikakannada");
+		Subject1.NumberOfChar("Digital signal pocessing");
+		Subject1.NumberOfChar("21EC42");
+	}
+
+}
